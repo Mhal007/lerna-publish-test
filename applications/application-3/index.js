ffc1e2920d4b18760bbe0b303fa1af5b54400ffc
Michał Weskida <michal.weskida@gmail.com>
2021-09-15 16:48:28 +0200

    dependency test

--- applications/application-3/index.js
+++ applications/application-3/index.js
@@ -1,3 +1,8 @@
 console.log('application-3');
 
+const application1 = require('@mhal007/application-1');
+const application2 = require('@mhal007/application-2');
+
+console.log('imported: ' + application1 + " " + application2)
+
 module.exports = "application-3";
