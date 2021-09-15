0ff1eb8190622a91f1aae7c273f94ea0e33ae6ce
Michał Weskida <michal.weskida@gmail.com>
2021-09-15 16:41:34 +0200

    feat: minor version change test

--- applications/application-1/__tests__/hello_world.js
+++ applications/application-1/__tests__/hello_world.js
@@ -2,6 +2,8 @@
 
 const application1 = require('../index.js');
 
+const test = 1;
+
 describe('application-1', () => {
     it('prints hello world', () => {
         console.log('hello world from application-1')
--- applications/application-2/__tests__/hello_world.js
+++ applications/application-2/__tests__/hello_world.js
@@ -2,6 +2,8 @@
 
 const application2 = require('../index.js');
 
+const test = 2;
+
 describe('application-2', () => {
     it('prints hello world', () => {
         console.log('hello world from application-2')
