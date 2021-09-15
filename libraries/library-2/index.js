999ab59cef05b04f5f33b744b0b4f366d50f3b3e
Michał Weskida <michal.weskida@gmail.com>
2021-09-15 17:51:26 +0200

    feat: something BREAKING CHANGE

--- libraries/library-2/index.js
+++ libraries/library-2/index.js
@@ -8,5 +8,6 @@ console.log('major change test 3');
 console.log('major change test 4');
 console.log('major change test 5');
 console.log('major change test 6');
+console.log('major change test 7');
 
 module.exports = "library-2";
