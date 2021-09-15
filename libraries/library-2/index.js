55702d0f2e2fb43a6e6cbb923b685eb322f7608d
Michał Weskida <michal.weskida@gmail.com>
2021-09-15 17:49:46 +0200

    feat: major changes test
    BREAKING CHANGE: test

--- libraries/library-2/index.js
+++ libraries/library-2/index.js
@@ -3,11 +3,9 @@ console.log('library-2');
 console.log('pathChange');
 
 console.log('major change test');
-
 console.log('major change test 2');
-
 console.log('major change test 3');
-
 console.log('major change test 4');
+console.log('major change test 5');
 
 module.exports = "library-2";
