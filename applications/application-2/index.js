db910c8ef1c18c1530d8e1dbea1ba00bcacae91b
Michał Weskida <michal.weskida@gmail.com>
2021-09-15 19:51:58 +0200

    feat: minor change 6

--- applications/application-2/index.js
+++ applications/application-2/index.js
@@ -4,5 +4,7 @@ console.log('application-2 minor change 2');
 console.log('application-2 minor change 3');
 console.log('application-2 minor change 4');
 console.log('application-2 minor change 5');
+console.log('application-2 minor change 6');
+console.log('application-2 minor change 7');
 
 module.exports = "application-2";
