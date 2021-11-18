7cc9d0aa9c7901885b8c93a2f1c2a14d444c0736
Michał Weskida <michal.weskida@gmail.com>
2021-11-18 12:57:20 +0100

    fix: adding lib1 as a dependency of app-3

--- applications/application-3/index.js
+++ applications/application-3/index.js
@@ -2,7 +2,8 @@ console.log('#### Hello World from application-3');
 
 const application1 = require('@mhal007/application-1');
 const application2 = require('@mhal007/application-2');
+const library1 = require('@mhal007/library-1');
 
-console.log('imported: ' + application1 + " " + application2)
+console.log('imported: ' + application1 + " " + application2 + " " + library1)
 
 module.exports = "application-3";
