000a072b53e8f1473930c5db75d14bcf5f890192
Michał Weskida <michal.weskida@gmail.com>
2022-07-08 14:46:04 +0200

    fix: turned husky back on

--- libraries/library-1/index.js
+++ libraries/library-1/index.js
@@ -1,15 +1,7 @@
 console.log('#### Hello World from library-1');
 
-const variable1 = 1;
-const variable2 = 2;
-const variable3 = 3;
-const variable4 = 4;
-const variable5 = 5;
-const variable6 = 6;
-const variable7 = 7;
-const variable8 = 8;
-const variable9 = 9;
-const variable10 = 10;
-const variable11 = 11;
+const variable = [
+  1
+]
 
 module.exports = "library-1";
